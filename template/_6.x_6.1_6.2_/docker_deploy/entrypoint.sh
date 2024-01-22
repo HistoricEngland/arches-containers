5bda458eff25e4bd86bb8ab9438ec1edb74a81f4
Andy Jones <andrew.jones@historicengland.org.uk>
2024-01-22 18:52:10 +0000

    adds v6 deployable docker file

---
+++ template/_6.x_6.1_6.2_/docker_deploy/entrypoint.sh
@@ -0,0 +1,306 @@
+#!/bin/bash
+
+# APP and YARN folder locations
+# ${WEB_ROOT} and ${ARCHES_ROOT} is defined in the Dockerfile, ${ARCHES_PROJECT} in env_file.env
+if [[ -z ${ARCHES_PROJECT} ]]; then
+	APP_FOLDER=${ARCHES_ROOT}
+	PACKAGE_JSON_FOLDER=${ARCHES_ROOT}
+else
+	APP_FOLDER=${WEB_ROOT}/${ARCHES_PROJECT}
+	PACKAGE_JSON_FOLDER=${ARCHES_ROOT}
+fi
+
+YARN_MODULES_FOLDER=${PACKAGE_JSON_FOLDER}/$(awk \
+	-F '--install.modules-folder' '{print $2}' ${PACKAGE_JSON_FOLDER}/.yarnrc \
+	| awk '{print $1}' \
+	| tr -d $'\r' \
+	| tr -d '"' \
+	| sed -e "s/^\.\///g")
+
+#Utility functions that check db status
+wait_for_db() {
+	echo "Testing if database server is up..."
+	while [[ ! ${return_code} == 0 ]]
+	do
+        psql --host=${PGHOST} --port=${PGPORT} --user=${PGUSERNAME} --dbname=postgres -c "select 1" >&/dev/null
+		return_code=$?
+		sleep 3
+	done
+	echo "Database server is up"
+
+    echo "Testing if Elasticsearch is up..."
+    while [[ ! ${return_code} == 0 ]]
+    do
+        curl -s "http://${ESHOST}:${ESPORT}/_cluster/health?wait_for_status=green&timeout=60s" >&/dev/null
+        return_code=$?
+        sleep 3
+    done
+    echo "Elasticsearch is up"
+}
+
+setup_postgis_template(){
+
+	echo "Creating PostGIS template"
+  	psql --host=${PGHOST} --port=${PGPORT} --user=${PGUSERNAME} --dbname=postgres -c "UPDATE pg_database SET datistemplate='false' WHERE datname='template_postgis';"
+   	psql --host=${PGHOST} --port=${PGPORT} --user=${PGUSERNAME} --dbname=postgres -c "DROP DATABASE IF EXISTS template_postgis;"
+	psql --host=${PGHOST} --port=${PGPORT} --user=${PGUSERNAME} --dbname=postgres -c "CREATE DATABASE template_postgis"
+	psql --host=${PGHOST} --port=${PGPORT} --user=${PGUSERNAME} --dbname=postgres -c "UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template_postgis'"
+	psql --host=${PGHOST} --port=${PGPORT} --user=${PGUSERNAME} --dbname=template_postgis -c "CREATE EXTENSION postgis"
+	psql --host=${PGHOST} --port=${PGPORT} --user=${PGUSERNAME} --dbname=template_postgis -c "CREATE EXTENSION postgis_topology"
+	psql --host=${PGHOST} --port=${PGPORT} --user=${PGUSERNAME} --dbname=template_postgis -c "GRANT ALL ON geometry_columns TO PUBLIC;"
+	psql --host=${PGHOST} --port=${PGPORT} --user=${PGUSERNAME} --dbname=template_postgis -c "GRANT ALL ON spatial_ref_sys TO PUBLIC;"
+	psql --host=${PGHOST} --port=${PGPORT} --user=${PGUSERNAME} --dbname=template_postgis -c "GRANT ALL ON geography_columns TO PUBLIC;"
+	psql --host=${PGHOST} --port=${PGPORT} --user=${PGUSERNAME} --dbname=template_postgis -c "CREATE EXTENSION IF NOT EXISTS 'uuid-ossp';"
+}
+
+db_exists() {
+	echo "Checking if database "${PGDBNAME}" exists..."
+	count=`psql --host=${PGHOST} --port=${PGPORT} --user=${PGUSERNAME} --dbname=postgres -Atc "SELECT COUNT(*) FROM pg_catalog.pg_database WHERE datname='${PGDBNAME}'"`
+
+	# Check if returned value is a number and not some error message
+	re='^[0-9]+$'
+	if ! [[ ${count} =~ $re ]] ; then
+	   echo "Error: Something went wrong when checking if database "${PGDBNAME}" exists..." >&2;
+	   echo "Exiting..."
+	   exit 1
+	fi
+
+	# Return 0 (= true) if database exists
+	if [[ ${count} > 0 ]]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+
+#### Install
+init_arches() {
+	echo "Checking if Arches project "${ARCHES_PROJECT}" exists..."
+	if [[ ! -d ${APP_FOLDER}/${ARCHES_PROJECT} ]] || [[ ! "$(ls ${APP_FOLDER}/${ARCHES_PROJECT})" ]]; then
+		echo ""
+		echo "----- Custom Arches project '${ARCHES_PROJECT}' does not exist. -----"
+		#echo "----- Use the "create_project" command to create the project and then restart the container -----"
+		echo ""
+		create_arches_project
+	else
+		echo "Custom Arches project '${ARCHES_PROJECT}' exists."
+	fi
+
+	wait_for_db
+	if db_exists; then
+		echo "Database ${PGDBNAME} already exists."
+		echo "Skipping Package Loading"
+	else
+		echo "Database ${PGDBNAME} does not exists yet."
+		setup_postgis_template
+		run_load_package #change to run_load_package if preferred
+	fi
+}
+
+create_arches_project() {
+	echo "Checking if Arches project "${ARCHES_PROJECT}" exists..."
+	if [[ ! -d ${APP_FOLDER}/${ARCHES_PROJECT} ]] || [[ ! "$(ls ${APP_FOLDER}/${ARCHES_PROJECT})" ]]; then
+		echo ""
+		echo "----- Creating '${ARCHES_PROJECT}'... -----"
+		echo ""
+
+		cd ${APP_FOLDER}
+		python3 ${WEB_ROOT}/arches/arches/install/arches-project create ${ARCHES_PROJECT} -d .
+		copy_settings_local
+		run_setup_db
+		setup_couchdb
+
+		exit_code=$?
+		if [[ ${exit_code} != 0 ]]; then
+			echo "Something went wrong when creating your Arches project: ${ARCHES_PROJECT}."
+			echo "Exiting..."
+			exit ${exit_code}
+		fi
+	else
+		echo "Custom Arches project '${ARCHES_PROJECT}' exists."
+	fi
+}
+
+# Setup Couchdb (when should this happen?)
+setup_couchdb() {
+    echo "Running: Creating couchdb system databases"
+    curl -X PUT ${COUCHDB_URL}/_users
+    curl -X PUT ${COUCHDB_URL}/_global_changes
+    curl -X PUT ${COUCHDB_URL}/_replicator
+}
+
+# Yarn
+install_yarn_components() {
+	if [[ ! -d ${YARN_MODULES_FOLDER} ]] || [[ ! "$(ls ${YARN_MODULES_FOLDER})" ]]; then
+		echo "Yarn modules do not exist, installing..."
+		cd ${PACKAGE_JSON_FOLDER}
+		yarn install
+	fi
+}
+
+#### Misc
+copy_settings_local() {
+	# The settings_local.py in ${ARCHES_ROOT}/arches/ gets ignored if running manage.py from a custom Arches project instead of Arches core app
+	echo "Copying ${WEB_ROOT}/docker/settings_local.py to ${APP_FOLDER}/${ARCHES_PROJECT}/settings_local.py..."
+	yes | cp ${WEB_ROOT}/docker/settings_local.py ${APP_FOLDER}/${ARCHES_PROJECT}/settings_local.py
+}
+
+#### Run commands
+
+start_celery_supervisor() {
+	echo "---- Starting Supervisord ----"
+	supervisord -c /etc/supervisor/arches_supervisord.conf
+}
+
+run_migrations() {
+	echo ""
+	echo "----- RUNNING DATABASE MIGRATIONS -----"
+	echo ""
+	cd ${APP_FOLDER}
+	python3 manage.py migrate
+}
+
+run_setup_db() {
+	echo ""
+	echo "----- RUNNING SETUP_DB -----"
+	echo ""
+	cd ${APP_FOLDER}
+	python3 manage.py setup_db --force
+}
+
+run_load_package() {
+	echo ""
+	echo "----- *** LOADING PACKAGE: ${ARCHES_PROJECT} *** -----"
+	echo ""
+	cd ${APP_FOLDER}
+	if [[ -d ${ARCHES_PROJECT}/pkg ]];then
+		python3 manage.py packages -o load_package -s ${ARCHES_PROJECT}/pkg -db -dev -y
+		setup_couchdb
+	fi
+}
+
+run_django_gunicorn(){
+	echo ""
+	echo "----- *** RUNNING DJANGO GUNICORN SERVER *** -----"
+	echo ""
+	cd ${APP_FOLDER}
+	echo "--- compress running...---"
+	python3 manage.py compress
+	echo "--- collectstatic running...---"
+	python3 manage.py collectstatic --noinput &
+	echo "--- starting gunicorn ---"
+	gunicorn warden.wsgi:application --bind 0.0.0.0:8000 --workers 3
+}
+
+
+# "exec" means that it will finish building???
+run_django_server() {
+	echo ""
+	echo "----- *** RUNNING DJANGO DEVELOPMENT SERVER *** -----"
+	echo ""
+	cd ${APP_FOLDER}
+    echo "Running Django on port ${DJANGO_PORT}"
+	#exec sh -c "pip install debugpy -t /tmp && python3 /tmp/debugpy --listen 0.0.0.0:5678 manage.py runserver 0.0.0.0:${DJANGO_PORT}"
+	python3 manage.py runserver 0.0.0.0:${DJANGO_PORT} --noreload
+}
+
+run_livereload_server() {
+	echo ""
+	echo "----- *** RUNNING LIVERELOAD SERVER *** -----"
+	echo ""
+	cd ${APP_FOLDER}
+    echo "Running livereload"
+    exec sh -c "python3 manage.py developer livereload --livereloadhost 0.0.0.0"
+}
+
+activate_virtualenv() {
+	. ${VENV}/bin/activate
+}
+
+#### Main commands
+run_arches() {
+	init_arches
+	install_yarn_components
+	#run_django_gunicorn
+	run_django_server
+}
+
+#### Main commands
+run_livereload() {
+	run_livereload_server
+}
+
+### Starting point ###
+
+# trying not to use virtualenv???
+activate_virtualenv
+
+# Use -gt 1 to consume two arguments per pass in the loop
+# (e.g. each argument has a corresponding value to go with it).
+# Use -gt 0 to consume one or more arguments per pass in the loop
+# (e.g. some arguments don't have a corresponding value to go with it, such as --help ).
+
+# If no arguments are supplied, assume the server needs to be run
+if [[ $#  -eq 0 ]]; then
+	start_celery_supervisor
+	wait_for_db
+	run_arches
+fi
+
+# Else, process arguments
+echo "Full command: $@"
+while [[ $# -gt 0 ]]
+do
+	key="$1"
+	echo "Command: ${key}"
+
+	case ${key} in
+		run_arches)
+			start_celery_supervisor
+			copy_settings_local
+			wait_for_db
+			run_arches
+		;;
+		run_arches_gunicorn)
+			start_celery_supervisor
+			copy_settings_local
+			wait_for_db
+			run_django_gunicorn
+		;;
+		run_livereload)
+			run_livereload_server
+		;;
+		setup_arches)
+			start_celery_supervisor
+			copy_settings_local
+			wait_for_db
+			setup_arches
+		;;
+		run_tests)
+			copy_settings_local
+			wait_for_db
+			run_tests
+		;;
+		run_migrations)
+			copy_settings_local
+			wait_for_db
+			run_migrations
+		;;
+		install_yarn_components)
+			install_yarn_components
+		;;
+		create_project)
+			create_arches_project
+		;;
+		help|-h)
+			display_help
+		;;
+		*)
+            cd ${APP_FOLDER}
+			"$@"
+			exit 0
+		;;
+	esac
+	shift # next argument or value
+done
