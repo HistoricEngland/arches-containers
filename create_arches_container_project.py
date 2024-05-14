1e76d353ae75fdc8db52d73a583f1a1b8310cd87
Rob Oates <rob.oates@historicengland.org.uk>
2024-05-14 08:53:58 +0000

    Initial commit. Unique naming, port numering and certificate introduction to get through Fortinet

--- create_arches_container_project.py
+++ create_arches_container_project.py
@@ -1,6 +1,7 @@
 import sys, os, inspect
 import argparse
 import shutil
+import re
 from slugify import slugify
 
 # copy directory and rename to first arg
@@ -8,6 +9,15 @@ from slugify import slugify
 context = os.path.dirname(os.path.abspath(inspect.getframeinfo(inspect.currentframe()).filename))
 replace_token = "{{project}}"
 replace_token_urlsafe = "{{project_urlsafe}}"
+
+# Get the directory of the currently running script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Get the parent directory
+parent_dir = os.path.dirname(script_dir)
+
+# Get the parent folder name
+parent_folder_name = os.path.basename(parent_dir)
 
 def get_target_path(project_name):
     return os.path.join(context,"projects",project_name)
@@ -22,9 +32,9 @@ def get_template_folder(version):
     return None
 
 def get_urlsafe_project_name(project_name):
-    return slugify(text=project_name, separator='')
+    return slugify(text=parent_folder_name + project_name, separator='')
 
-def create_proj_directory(project_name, version):
+def create_proj_directory(project_name, version, port_incrementor):
     target_path = get_target_path(project_name)
     template_folder = get_template_folder(version)
     
@@ -35,10 +45,10 @@ def create_proj_directory(project_name, version):
         raise Exception(f"ERROR: project path exists: {target_path}")
     
     shutil.copytree(template_folder, target_path)
-    replace_projectname_placeholder(project_name)
+    replace_projectname_placeholder(project_name, port_incrementor)
     return target_path
     
-def replace_projectname_placeholder(project_name):
+def replace_projectname_placeholder(project_name, port_incrementor):
     for dname, dirs, files in os.walk(get_target_path(project_name)):
         for fname in files:
             fpath = os.path.join(dname, fname)
@@ -46,14 +56,23 @@ def replace_projectname_placeholder(project_name):
                 s = f.read()
             s = s.replace(replace_token, project_name)
             s = s.replace(replace_token_urlsafe, get_urlsafe_project_name(project_name))
+            s = increment_port_number(s, port_incrementor)
             with open(fpath, "w") as f:
                 f.write(s)
+
+def increment_port_number(s, port_incrementor):
+    numbers = re.findall(r'{{(\d+)}}', s)
+    for number in numbers:
+            incremented_number = str(int(number) + int(port_incrementor))
+            s = s.replace('{{' + number + '}}', incremented_number)
+    return s
 
 def handle_create_container_project():
     project_name = slugify(text=args.project_name, separator='_')
     version = args.version.lower()
+    port_incrementor = args.port_incrementor if args.port_incrementor else 0
     print(f"Creating container project {project_name}")
-    print(f"... path: {create_proj_directory(project_name, version)}")
+    print(f"... path: {create_proj_directory(project_name, version, port_incrementor)}")
     print(f"{'-' * 5} Completed {'-' * 5}")
     
     
@@ -64,8 +83,9 @@ parser = argparse.ArgumentParser(prog="Arches Container Project Generator",
                                  description="Creates an arches container project that sets up all the required files and default values for running an Arches instance in Docker using docker-compose")
 
 # main requirement is the project name
-parser.add_argument("-p","--project_name", required=True , help="The name of the project. This value will be slugified to lowercase with underscore seperators")
-parser.add_argument("-v","--version", required=True , help="The arches version the project will be using (major.minor format)")
+parser.add_argument("-p","--project_name", required=True, help="The name of the project. This value will be slugified to lowercase with underscore seperators")
+parser.add_argument("-v","--version", required=True, help="The arches version the project will be using (major.minor format)")
+parser.add_argument("-pi", "--port_incrementor", required=False, help="Increments external facing port numbers by this value. Default is 0. Useful for running multiple projects on the same host.")
 args = parser.parse_args()
 
 if __name__ == '__main__':
